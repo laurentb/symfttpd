e72f853dc12767926787fe15bddd298d123c7bd2
Laurent Bachelier <laurent@bachelier.name>
2010-12-23 09:09:31 +0100

    Rationalize output messages
    Everything uses log_message() instead of echo.
    log_message() is now able to output to stderr.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,10 +1,32 @@
 <?php
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', true);
+
+/**
+ * Display a message on the standard output
+ * @param string $message
+ * @param boolean $error Treat the message as an error.
+ *
+ * @author Laurent Bachelier <laurent@bachelier.name>
+ */
+function log_message($message, $error = false)
+{
+  if ($error)
+  {
+    file_put_contents('php://stderr', $message."\n");
+  }
+  else
+  {
+    echo $message."\n";
+  }
+}
+
+/* Needed for some weird reason; PHP will not display anything
+ * when there is an uncatched exception. */
 function handle_exception($e)
 {
-  echo "Fatal error:\n";
-  echo $e."\n";
+  log_message('Fatal error:', true);
+  log_message($e, true);
   exit(1);
 }
 set_exception_handler('handle_exception');
