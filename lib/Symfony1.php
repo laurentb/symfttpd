ebbc001e126b61904cc7a35ce052aa4b1f690ee4
Laurent Bachelier <laurent@bachelier.name>
2011-02-12 13:13:42 +0100

    More factorization for symlink handling

--- lib/Symfony1.php
+++ lib/Symfony1.php
@@ -1,13 +1,17 @@
 <?php
 class Symfony1 extends Application
 {
+  protected $version;
+
   /**
    * TODO if not in the root of the project, try to find it (like git does)
+   * @param string $path Project root path. If not provided, try current directory.
+   * @param string $version Symfony version: '1.0', '1.1'…
    * @see Application::__construct
    *
    * @author Laurent Bachelier <laurent@bachelier.name>
    */
-  public function __construct($project_path = null)
+  public function __construct($project_path = null, $version)
   {
     if ($project_path === null)
     {
@@ -20,5 +24,48 @@ class Symfony1 extends Application
     }
 
     $this->project_path = $project_path;
+    $this->version = $version;
   }
+
+  /**
+   * Find plugins with a "web" directory
+   * @param string $project_path
+   * @return array Plugin names
+   *
+   * @author Laurent Bachelier <laurent@bachelier.name>
+   */
+  public function findPlugins()
+  {
+    $plugins = array();
+    foreach (new DirectoryIterator($this->getProjectPath()."/plugins") as $file)
+    {
+      $name = $file->getFilename();
+      if ($file->isDir()
+          && preg_match('/^[^\.].+Plugin$/', $name)
+          && is_dir($this->getProjectPath().'/plugins/'.$name.'/web')
+      )
+      {
+        $plugins[] = $name;
+      }
+    }
+
+    return $plugins;
+  }
+
+  /**
+   * @return array
+   *
+   * @author Laurent Bachelier <laurent@bachelier.name>
+   */
+  public function getAvailableLinks()
+  {
+
+    return array(
+      'symfony_symlink' => '',
+      'lib_symlink' => 'lib',
+      'data_symlink' => 'data',
+      'web_symlink' => 'data/web/sf',
+    );
+  }
+
 }
