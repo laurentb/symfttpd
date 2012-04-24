803216eb80be8c8058475e7502a8a3a15d5876c0
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-04-24 23:41:37 +0200

    [refactoring] Use composer.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,5 @@
  * @since 24/10/2011
  */
 
-require_once __DIR__.'/../lib/vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony'  => __DIR__.'/../lib/vendor',
-    'Symfttpd' => array(__DIR__.'/../lib/', __DIR__),
-));
-
-$loader->register();
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('Symfttpd', array(__DIR__.'/../lib/', __DIR__));
