e714d4d87e2f1c3a5074da5e74d2aa1bc9616dc8
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-04-29 12:45:38 +0200

    [refactoring] Add Symfttpd application.

---
+++ lib/Symfttpd/Console/Application.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the Symfttpd Project
+ *
+ * (c) Laurent Bachelier <laurent@bachelier.name>
+ * (c) Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfttpd\Console;
+
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfttpd\Symfttpd;
+
+/**
+ * Application class
+ *
+ * @author Benjamin Grandfond <benjaming@theodo.fr>
+ */
+class Application extends BaseApplication
+{
+    /**
+     * @var Symfttpd\Symfttpd
+     */
+    protected $symfttpd;
+
+    public function __construct()
+    {
+        parent::__construct('Symfttpd', Symfttpd::VERSION);
+    }
+
+    /**
+     * Return Symfttpd
+     *
+     * @return \Symfttpd\Symfttpd
+     */
+    public function getSymfttpd()
+    {
+        if (null == $this->symfttpd) {
+
+            $this->symfttpd = new Symfttpd();
+        }
+
+        return $this->symfttpd;
+    }
+
+    /**
+     * @param \Symfttpd\Symfttpd $symfttpd
+     */
+    public function setSymfftpd(Symfttpd $symfttpd)
+    {
+        $this->symfttpd = $symfttpd;
+    }
+}
