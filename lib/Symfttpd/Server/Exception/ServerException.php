499da5d2b1036470cb322b0413d040f1eea34f32
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-05-01 23:00:04 +0200

    [refactoring] Forgot these files.

---
+++ lib/Symfttpd/Server/Exception/ServerException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the Symfttpd Project
+ *
+ * (c) Laurent Bachelier <laurent@bachelier.name>
+ * (c) Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfttpd\Server\Exception;
+
+/**
+ * ServerException class
+ *
+ * @author Benjamin Grandfond <benjaming@theodo.fr>
+ * @since 01/05/12
+ */
+class ServerException extends \Exception
+{
+
+}
