77009ac45960a067b8a9784456e84527acaf70e5
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-05-08 23:22:03 +0200

    [phar] Add phar compiler.

---
+++ bin/compile.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+/**
+ * This file is part of the Symfttpd Project
+ *
+ * (c) Laurent Bachelier <laurent@bachelier.name>
+ * (c) Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfttpd\Compiler;
+
+$compiler = new Compiler();
+$compiler->compile();
---
+++ lib/Symfttpd/Compiler.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * This file is part of the Symfttpd Project
+ *
+ * (c) Laurent Bachelier <laurent@bachelier.name>
+ * (c) Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Symfttpd;
+
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Compiler class
+ *
+ * @author Benjamin Grandfond <benjaming@theodo.fr>
+ */
+class Compiler
+{
+    public function compile($pharFile = 'symfttpd.phar')
+    {
+        if (file_exists($pharFile)) {
+            unlink($pharFile);
+        }
+
+        $phar = new \Phar($pharFile, 0, 'symfttpd.phar');
+        $phar->setSignatureAlgorithm(\Phar::SHA1);
+
+        $phar->startBuffering();
+
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->notName('Compiler.php')
+            ->in(__DIR__.'/..')
+            ->in(__DIR__.'/../../vendor/symfony')
+            ->exclude('Tests');
+
+        foreach ($finder as $file) {
+            $this->addFile($phar, $file);
+        }
+
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_namespaces.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_classmap.php'));
+        $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/ClassLoader.php'));
+        $this->addSymfttpdBin($phar);
+
+        // Stubs
+        $phar->setStub($this->getStub());
+
+        $phar->stopBuffering();
+    }
+
+    private function addFile(\Phar $phar, $file)
+    {
+        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
+
+        $content = file_get_contents($file);
+
+        $phar->addFromString($path, $content);
+    }
+
+    private function addSymfttpdBin($phar)
+    {
+        $content = file_get_contents(__DIR__.'/../../bin/symfttpd');
+        $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+        $phar->addFromString('bin/symfttpd', $content);
+    }
+
+    private function getStub()
+    {
+        return <<<'EOF'
+#!/usr/bin/env php
+<?php
+/**
+ * This file is part of the Symfttpd Project
+ *
+ * (c) Laurent Bachelier <laurent@bachelier.name>
+ * (c) Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+Phar::mapPhar('symfttpd.phar');
+
+require 'phar://symfttpd.phar/bin/symfttpd';
+
+__HALT_COMPILER();
+EOF;
+    }
+}
