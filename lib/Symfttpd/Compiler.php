a10ff65289afe412e36c319011adacb213496863
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-05-14 23:07:10 +0200

    [Compiler] Add Twig in the phar.

--- lib/Symfttpd/Compiler.php
+++ lib/Symfttpd/Compiler.php
@@ -16,6 +16,10 @@ use Symfony\Component\Finder\Finder;
 /**
  * Compiler class
  *
+ * Partly taken from Composer and Silex compiler class.
+ * @see https://github.com/composer/composer/blob/master/src/Composer/Compiler.php
+ * @see https://github.com/fabpot/Silex/blob/master/src/Silex/Compiler.php
+ *
  * @author Benjamin Grandfond <benjaming@theodo.fr>
  */
 class Compiler
@@ -31,20 +35,35 @@ class Compiler
 
         $phar->startBuffering();
 
+        // Add Symfttpd core files.
         $finder = new Finder();
         $finder->files()
             ->ignoreVCS(true)
             ->name('*.php')
-            ->name('*.conf')
-            ->notName('Compiler.php')
+            ->name('*.twig')
             ->in(__DIR__.'/..')
-            ->in(__DIR__.'/../../vendor/symfony')
+            ->notName('Compiler.php')
             ->exclude('Tests');
+
+        foreach ($finder as $file) {
+            $this->addFile($phar, $file, false);
+        }
+
+        // Add vendors
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->in(__DIR__.'/../../vendor/symfony')
+            ->in(__DIR__.'/../../vendor/twig')
+            ->exclude(__DIR__.'/../../vendor/twig/doc')
+            ->exclude(__DIR__.'/../../vendor/twig/test');
 
         foreach ($finder as $file) {
             $this->addFile($phar, $file);
         }
 
+        // Add Composer generated files.
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/autoload.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_namespaces.php'));
         $this->addFile($phar, new \SplFileInfo(__DIR__.'/../../vendor/composer/autoload_classmap.php'));
@@ -57,11 +76,14 @@ class Compiler
         $phar->stopBuffering();
     }
 
-    private function addFile(\Phar $phar, $file)
+    private function addFile(\Phar $phar, $file, $strip = true)
     {
         $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
 
         $content = file_get_contents($file);
+        if ($strip) {
+            $content = $this->stripWhitespace($content);
+        }
 
         $phar->addFromString($path, $content);
     }
@@ -71,6 +93,41 @@ class Compiler
         $content = file_get_contents(__DIR__.'/../../bin/symfttpd');
         $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
         $phar->addFromString('bin/symfttpd', $content);
+    }
+
+
+    /**
+     * Removes whitespace from a PHP source string while preserving line numbers.
+     *
+     * @param string $source A PHP string
+     * @return string The PHP string with the whitespace removed
+     */
+    private function stripWhitespace($source)
+    {
+        if (!function_exists('token_get_all')) {
+            return $source;
+        }
+
+        $output = '';
+        foreach (token_get_all($source) as $token) {
+            if (is_string($token)) {
+                $output .= $token;
+            } elseif (in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
+                $output .= str_repeat("\n", substr_count($token[1], "\n"));
+            } elseif (T_WHITESPACE === $token[0]) {
+                // reduce wide spaces
+                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                // normalize newlines to \n
+                $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
+                // trim leading spaces
+                $whitespace = preg_replace('{\n +}', "\n", $whitespace);
+                $output .= $whitespace;
+            } else {
+                $output .= $token[1];
+            }
+        }
+
+        return $output;
     }
 
     private function getStub()
