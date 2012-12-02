5bc4368d99d4eef6db918b2de1b1feb98849df00
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2012-12-02 17:15:02 +0100

    Liskov principle

--- lib/Symfttpd/Command/SpawnCommand.php
+++ lib/Symfttpd/Command/SpawnCommand.php
@@ -66,9 +66,10 @@ class SpawnCommand extends Command
     /**
      * Run the Symttpd configured server.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface   $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface $output
-     * @throws \InvalidArgumentException
+     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
